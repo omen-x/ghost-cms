302a621b954e0f2b1ee828ebb15ab9f78bcf04d9
Denis Volok <denis.omen.x@gmail.com>
2019-12-16 03:54:33 +0300

    refactor: misc refactor

--- src/server/router/login.ts
+++ src/server/router/login.ts
@@ -6,16 +6,21 @@ import { CommonError } from '../utils/errors';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res): void => {
   res.sendFile('login.html', { root: './build/public' });
 });
 
-router.post('/', (req, res, next) => {
-  passport.authenticate('local', (err, user) => {
+router.post('/', (req, res, next): void => {
+  passport.authenticate('local', (err, user): void => {
     if (err) return next(err);
-    if (!user) return next(new CommonError({ clientMessage: 'User not found' }));
+    if (!user) {
+      return next(new CommonError({
+        message: 'User not found',
+        clientMessage: 'User not found',
+      }));
+    }
 
-    req.login(user, errLogin => {
+    req.login(user, (errLogin): void => {
       if (errLogin) return next(errLogin);
 
       logger.debug('Success user auth');
