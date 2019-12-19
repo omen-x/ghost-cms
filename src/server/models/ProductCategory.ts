eaebdef51f0bd12deea8016514e5f5b3ac11462e
Denis Volok <denis.omen.x@gmail.com>
2019-12-19 04:24:02 +0300

    feat(category): implement product category model

---
+++ src/server/models/ProductCategory.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import { CommonError } from '../utils/errors';
+
+
+interface ProductCategory {
+  name: string;
+  parentCategoryID?: mongoose.Schema.Types.ObjectId;
+}
+
+interface ProductCategoryModel extends ProductCategory, mongoose.Document {}
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  parentCategoryID: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+});
+
+
+export const ProductCategory = mongoose.model<ProductCategoryModel>('ProductCategory', schema);
+
+
+schema.pre('save', function pre(next): void {
+  const parentCategoryID = this.get('parentCategoryID');
+
+  if (parentCategoryID) {
+    ProductCategory.findById(parentCategoryID, (err, cat): void => {
+      if (err) return next(err);
+      if (!cat) return next(new CommonError({ message: 'category not found' }));
+
+      return next();
+    });
+  } else {
+    next();
+  }
+});
